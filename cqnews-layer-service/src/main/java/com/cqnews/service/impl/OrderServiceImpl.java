6fd90f9448f3e7b1aa3405ee7f9ea78f84ecc623
‘yaoxiang <yaoxiang@cqliving.com>
2023-09-27 16:23:05 +0800

    MVC分层架构设计

---
+++ cqnews-layer-service/src/main/java/com/cqnews/service/impl/OrderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cqnews.service.impl;
+
+import com.cqnews.api.OrderService;
+
+/**
+ * @description:
+ * @author: uYxUuu actionYx1998@163.com
+ * @createTime:2023/9/27 16:22
+ */
+public class OrderServiceImpl implements OrderService {
+}
---
+++ cqnews-layer-service/src/main/java/com/cqnews/service/rpc/OrderProvider.java
@@ -0,0 +1,11 @@
+package com.cqnews.service.rpc;
+
+import com.cqnews.service.impl.OrderServiceImpl;
+
+/**
+ * @description:
+ * @author: uYxUuu actionYx1998@163.com
+ * @createTime:2023/9/27 16:22
+ */
+public class OrderProvider extends OrderServiceImpl {
+}
